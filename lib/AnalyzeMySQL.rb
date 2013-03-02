820e6b1cbd3937421ef0edf75cea470c8d88ce36
Mario Mueller <mario@xenji.com>
2013-03-02 17:24:00 +0100

    Started executioner

--- lib/AnalyzeMySQL.rb
+++ lib/AnalyzeMySQL.rb
@@ -1,5 +1,22 @@
 require 'AnalyzeMySQL/version'
 require 'AnalyzeMySQL/structure'
 module AnalyzeMySQL
+  def configure(params)
 
+  end
+
+  def exec(conf)
+    conf['databases'].each do |db|
+      acc = conf['access'][db]
+      conn = Mysql2::Client.new(
+          :host => acc['host'],
+          :username => acc['user'],
+          :password => acc['pass'],
+          :port => acc['port'],
+          :flags => Mysql2::Client::MULTI_STATEMENTS
+      )
+
+      # Do smth
+    end
+  end
 end
